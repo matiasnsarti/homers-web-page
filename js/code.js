61ae51f27739fd5bc51dc336e3c04654970e3600
Matías Nicolás Sarti <66924675+matiasnsarti@users.noreply.github.com>
2021-06-07 23:03:52 -0300

    commit audio

--- js/code.js
+++ js/code.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', function(){
     const loader = document.getElementById('loader');
+    const audio = document.body.querySelector('audio');
     this.setTimeout(function(){
         loader.classList.toggle('loader2');
-        document.body.querySelector('audio').play();
+        audio.play();
     }, 3000)
 })
