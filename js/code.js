6536b2715241087d1e3e11c63eaefaa537f4e882
Matías Nicolás Sarti <66924675+matiasnsarti@users.noreply.github.com>
2021-06-07 22:55:46 -0300

    commit loader

--- js/code.js
+++ js/code.js
@@ -2,6 +2,6 @@ window.addEventListener('load', function(){
     const loader = document.getElementById('loader');
     this.setTimeout(function(){
         loader.classList.toggle('loader2');
+        document.body.querySelector('audio').play();
     }, 3000)
-    
 })
